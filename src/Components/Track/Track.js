6cb0ff03ac277d0bdc64bd1ebf77701aa3f60953
Katie Cannon <katiecannon@katies-air.lan>
2020-05-24 13:07:50 +0100

    displaying track info from props

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -9,8 +9,8 @@ class Track extends React.Component{
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3><!-- track name will go here --></h3>
-                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                    <h3>{this.props.track.name</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 <button className="Track-action">{renderAction()}</button>
             </div>
