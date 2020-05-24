b3d8d4058c1be92cafaeb23e167bb71fab66b6c4
Katie Cannon <katiecannon@katies-air.lan>
2020-05-24 13:08:10 +0100

    passing track info

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './TrackList.css';
+import {Track} from './Track/Track';
 
 class TrackList extends React.Component{
     render(){
         return (
             <div className="TrackList">
-                 <!-- You will add a map method that renders a set of Track components  -->
+                 {this.props.tracks.map(track => {
+                     return <Track key={track.id} track={track}/>
+                 })}
             </div>
         )
     }
