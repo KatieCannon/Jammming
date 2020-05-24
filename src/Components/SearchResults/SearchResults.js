946e79208ed4893f98b000fb3404cab7fb006836
Katie Cannon <katiecannon@katies-air.lan>
2020-05-24 13:07:23 +0100

    tracks passed to Tracklist

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './SearchResults.css';
+import Tracklist from './Tracklist/Tracklist';
 
 class SearchResults extends React.Component{
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                <!-- Add a TrackList component -->
+                <Tracklist tracks={this.props.searchResults}/>
             </div>
         )
     }
