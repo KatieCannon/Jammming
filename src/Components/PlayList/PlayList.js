8c70c898e0d2a9cba94d9e12e37349fb6d45ef4b
Katie Cannon <katiecannon@katies-air.lan>
2020-05-25 10:55:12 +0100

    correction to passing boolean

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -7,7 +7,7 @@ class PlayList extends React.Component{
         return(
             <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
-                <TrackList tracks={this.props.playListTracks} onRemove={this.props.onRemove} isRemoval='true'/>
+                <TrackList tracks={this.props.playListTracks} onRemove={this.props.onRemove} isRemoval={true}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
